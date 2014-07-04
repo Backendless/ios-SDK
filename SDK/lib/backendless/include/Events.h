e8a0da264ee52c54a1ea7018f9ea23a3db38b1b2
Vyacheslav Vdovichenko <slavav@themidnightcoders.com>
2014-07-04 13:50:16 +0300

    added /private-keys

---
+++ SDK/lib/backendless/include/Events.h
@@ -0,0 +1,29 @@
+//
+//  Events.h
+//  backendlessAPI
+/*
+ * *********************************************************************************************************************
+ *
+ *  BACKENDLESS.COM CONFIDENTIAL
+ *
+ *  ********************************************************************************************************************
+ *
+ *  Copyright 2012 BACKENDLESS.COM. All Rights Reserved.
+ *
+ *  NOTICE: All information contained herein is, and remains the property of Backendless.com and its suppliers,
+ *  if any. The intellectual and technical concepts contained herein are proprietary to Backendless.com and its
+ *  suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret
+ *  or copyright law. Dissemination of this information or reproduction of this material is strictly forbidden
+ *  unless prior written permission is obtained from Backendless.com.
+ *
+ *  ********************************************************************************************************************
+ */
+
+#import <Foundation/Foundation.h>
+@protocol IResponder;
+@class Fault;
+@interface Events : NSObject
+-(NSDictionary *)dispatchEventName:(NSString *)name args:(NSDictionary *)eventArgs fault:(Fault **)fault;
+-(void)dispatchEventName:(NSString *)name args:(NSDictionary *)eventArgs responder:(id <IResponder>)responder;
+-(void)dispatchEventName:(NSString *)name args:(NSDictionary *)eventArgs response:(void(^)(NSDictionary *data))responseBlock error:(void(^)(Fault *fault))errorBlock;
+@end
