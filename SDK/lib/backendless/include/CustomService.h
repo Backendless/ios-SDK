4bc9fdc7c7aff344eef7b91c656ebd08bb243d3a
Vyacheslav Vdovichenko <slavav@Vyacheslavs-MacBook-Pro.local>
2014-10-10 15:22:07 +0300

    libav-v9.1965 & MediaLibiOS2x were removed from lib folder

---
+++ SDK/lib/backendless/include/CustomService.h
@@ -0,0 +1,36 @@
+//
+//  CustomService.h
+//  backendlessAPI
+/*
+ * *********************************************************************************************************************
+ *
+ *  BACKENDLESS.COM CONFIDENTIAL
+ *
+ *  ********************************************************************************************************************
+ *
+ *  Copyright 2014 BACKENDLESS.COM. All Rights Reserved.
+ *
+ *  NOTICE: All information contained herein is, and remains the property of Backendless.com and its suppliers,
+ *  if any. The intellectual and technical concepts contained herein are proprietary to Backendless.com and its
+ *  suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret
+ *  or copyright law. Dissemination of this information or reproduction of this material is strictly forbidden
+ *  unless prior written permission is obtained from Backendless.com.
+ *
+ *  ********************************************************************************************************************
+ */
+
+#import <Foundation/Foundation.h>
+
+@protocol IResponder;
+@class Fault;
+
+@interface CustomService : NSObject
+// sync methods with fault return (as exception)
+-(id)invoke:(NSString *)serviceName serviceVersion:(NSString *)serviceVersion method:(NSString *)method args:(NSArray *)args;
+// sync methods with fault option
+-(id)invoke:(NSString *)serviceName serviceVersion:(NSString *)serviceVersion method:(NSString *)method args:(NSArray *)args fault:(Fault **)fault;
+// async methods with responder
+-(void)invoke:(NSString *)serviceName serviceVersion:(NSString *)serviceVersion method:(NSString *)method args:(NSArray *)args responder:(id<IResponder>)responder;
+// async methods with block-based callbacks
+-(void)invoke:(NSString *)serviceName serviceVersion:(NSString *)serviceVersion method:(NSString *)method args:(NSArray *)args response:(void(^)(id))responseBlock error:(void(^)(Fault *fault))errorBlock;
+@end
