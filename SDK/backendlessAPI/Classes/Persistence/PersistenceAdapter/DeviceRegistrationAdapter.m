bd952461afaf5733b70a928e7a4c6b45999e937c
Olga Danylova <olga@themidnightcoders.com>
2018-03-27 18:37:15 +0300

    Фвфзеукы уккщк рфтвдштп ашчув

--- SDK/backendlessAPI/Classes/Persistence/PersistenceAdapter/DeviceRegistrationAdapter.m
+++ SDK/backendlessAPI/Classes/Persistence/PersistenceAdapter/DeviceRegistrationAdapter.m
@@ -33,23 +33,28 @@
 @implementation DeviceRegistrationAdapter
 
 -(id)adapt:(id)type {
-    V3Message *v3 = (V3Message *)[type defaultAdapt];
-    if (v3.isError) {
-        ErrMessage *result = (ErrMessage *)v3;
-        return [Fault fault:result.faultString detail:result.faultDetail faultCode:result.faultCode];
-    }
     NSMutableDictionary *typeProperties = ((AnonymousObject *)[type getCacheKey]).properties;
-    id body = [typeProperties valueForKey:@"body"];    
-    if ([body isKindOfClass:[NamedObject class]]) {
-        return [self adaptToDeviceRegistration:body];
+    if ([typeProperties valueForKey:@"faultCode"] ||
+        [typeProperties valueForKey:@"faultDetail"] ||
+        [typeProperties valueForKey:@"faultString"]) {
+        Fault *fault = [[Fault alloc] initWithMessage:[[typeProperties valueForKey:@"faultString"] defaultAdapt]
+                                               detail:[[typeProperties valueForKey:@"faultDetail"] defaultAdapt]
+                                            faultCode:[[typeProperties valueForKey:@"faultCode"] defaultAdapt]];
+        return fault;
     }
-    else if ([body isKindOfClass:[ArrayType class]]) {
-        NSMutableArray *result = [NSMutableArray new];
-        NSArray *bodyObjects = [body getArray];
-        for (NamedObject *bodyObject in bodyObjects) {
-            [result addObject:[self adaptToDeviceRegistration:bodyObject]];
+    else {
+        id body = [typeProperties valueForKey:@"body"];
+        if ([body isKindOfClass:[NamedObject class]]) {
+            return [self adaptToDeviceRegistration:body];
         }
-        return result;
+        else if ([body isKindOfClass:[ArrayType class]]) {
+            NSMutableArray *result = [NSMutableArray new];
+            NSArray *bodyObjects = [body getArray];
+            for (NamedObject *bodyObject in bodyObjects) {
+                [result addObject:[self adaptToDeviceRegistration:bodyObject]];
+            }
+            return result;
+        }
     }
     return nil;
 }
